c581d2f7e5e1e9e586479a5b79983842486e6742
Qvenskaya <qvenskaya@gmail.com>
2023-07-27 19:38:55 +0700

    delete file

--- delete_file.py
+++ delete_file.py
@@ -1,8 +1,8 @@
 def delete_entry():
     name = input("Input full name to delete contact: ")
-    with open("phonebook.txt", "r", encoding="utf-8") as file:
+    with open("phonebook.txt", "r", encoding="UTF-8") as file:
         lines = file.readlines()
-    with open("phonebook.txt", "w", encoding="utf-8") as file:
+    with open("phonebook.txt", "w", encoding="UTF-8") as file:
         file.write(lines[0])
         for line in lines[1:]:
             if name.lower() not in line.lower():
